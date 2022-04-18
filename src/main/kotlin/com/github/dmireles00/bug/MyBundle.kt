6eb6108d1f1bfa415f03d948e4a78a31d1baf8b2
GitHub Action <action@github.com>
2022-04-18 23:23:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/dmireles00/bug/MyBundle.kt
+++ src/main/kotlin/com/github/dmireles00/bug/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dmireles00.bug
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/dmireles00/bug/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dmireles00/bug/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dmireles00.bug.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dmireles00.bug.MyBundle
 
 class MyApplicationService {
 
